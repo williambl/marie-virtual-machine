d4b719f0d7f3391304dea2c0922d544fc2605cfb
williambl <william@williambl.com>
2021-11-09 12:24:41 +0000

    Add main command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,14 @@
-fun main(args: Array<String>) {
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.types.path
+import kotlin.io.path.readBytes
+
+class MarieMachine: CliktCommand() {
+    private val input by argument(help="Assembled MARIE code").path(canBeDir = false, mustBeReadable = true)
+
+    override fun run() {
+        Machine(input.readBytes())
+    }
 }
+
+fun main(args: Array<String>) = MarieMachine().main(args)
